9d977ab63813d11c784941a5f0d06c2c732ae07f
aq032 <anthonyq032@gmail.com>
2022-08-10 12:45:37 -0700

    Modified manager_ids

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,7 @@
 INSERT INTO departments (dep_name)
 VALUES 
 ("Engineering"),
-("Financing"),
+("Finance"),
 ("Legal"),
 ("Sales"),
 ("Service");
@@ -15,22 +15,19 @@ VALUES
 ("Account Manager", 72000.00, 2),
 ("Accountant", 54000.00, 2),
 ("Legal Team Lead", 232000.00, 3),
-("Lawyer", 160000.00, 3),
-("Customer Service", 40000.00, 5);
+("Lawyer", 160000.00, 3);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES 
-("Chai", "Suksanguan", 4, NULL),
-("Ivan", "Furlong", 4, NULL),
-("Luis", "Escobar", 2, NULL),
-("Manuel", "Gavino", 1, 4),
-("Hideo", "Kojima", 3, 5),
-("Cesar", "Infante", 5, 6),
-("Gilberto", "Escobedo", 6, NULL),
-("Nate", "McIlvenny", 8, NULL),
-("Skyler", "Ferreira", 9, NULL),
-("Diana", "Contreras", 2, NULL),
-("Thomas", "Long", 2, NULL),
-("Daniel", "Cooper", 9, NULL),
-("Kimberly", "Wexler", 8, NULL),
-("James", "McGill", 7, 14);
+("Hideo", "Kojima", 3, NULL),
+("Chai", "Suksanguan", 4, 1),
+("Ivan", "Furlong", 4, 1),
+("Manuel", "Gavino", 1, NULL),
+("Luis", "Escobar", 2, 4),
+("Cesar", "Infante", 5, NULL),
+("Gilberto", "Escobedo", 6, 6),
+("Diana", "Contreras", 2, 4),
+("Thomas", "Long", 2, 4),
+("James", "McGill", 7, NULL),
+("Nate", "McIlvenny", 8, 10),
+("Kimberly", "Wexler", 8, 10);
